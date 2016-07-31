70d0ba8af9d803931e73905cadf449cd10cb0816
Alex M.A.K <alex-m.a.k@yandex.kz>
2016-07-31 12:15:09 +0600

    исправил работу виджета filesystem

--- widgets/filesystem/init.lua
+++ widgets/filesystem/init.lua
@@ -12,13 +12,12 @@ then
 	        widget:set_markup(markup("#80d9d8", fs_now.available .. "% "))
 	    end
 	})
-else
-	 -- | FS | --
-	fs_widget = wibox.widget.textbox()
-	vicious.register(fs_widget, vicious.widgets.fs, vspace1 .. "${/ avail_gb}GB" .. vspace1, 2)
-	widget_fs = wibox.widget.imagebox()
-	widget_fs:set_image(beautiful.widget_fs)
-	fswidget = wibox.widget.background()
-	fswidget:set_widget(fs_widget)
-	fswidget:set_bgimage(beautiful.widget_display)
 end
+  -- | FS | --
+fs_widget = wibox.widget.textbox()
+vicious.register(fs_widget, vicious.widgets.fs, vspace1 .. "${/home avail_gb}GB" .. vspace1, 2)
+widget_fs = wibox.widget.imagebox()
+widget_fs:set_image(beautiful.widget_fs)
+fswidget = wibox.widget.background()
+fswidget:set_widget(fs_widget)
+fswidget:set_bgimage(beautiful.widget_display)
